a61346da9f69c01eb09fa29bbba5195422bd4c99
mark novelero <mark.novelero@gmail.com>
2021-04-12 16:55:41 -0400

    commit tree 6b1db4eca0756fd176b5e214e2944117491e3f56

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateText()
+  } );
+  
+  
+  function updateText() {
+    let getP = document.getElementById( "text" )
+      getP.innerHTML = "This is really cool!";
+  }
+
